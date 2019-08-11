32627d556a274ba4d37c85bb24e50a05ccc42f38
joffrehermosilla <joffre.hermosilla@gmail.com>
2019-08-10 22:47:17 -0500

    Segundo Commit prueba Admin Joffre

--- joffre/src/main/java/joffre/NanifarfallaRest/model/Insumo.java
+++ joffre/src/main/java/joffre/NanifarfallaRest/model/Insumo.java
@@ -1,5 +1,19 @@
 package joffre.NanifarfallaRest.model;
 
-public class Insumo {
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "insumo")
+@EntityListeners(AuditingEntityListener.class)
+public class Insumo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo_insumo;
 }
